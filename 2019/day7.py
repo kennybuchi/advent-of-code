a1c8145ef32e760083e60de01a39187ed3b92d6a
Kenneth Lee <kenny.lee@moesol.com>
2019-12-11 18:04:51 -0800

    2019 - day 7 pt 2 bug fix

--- 2019/day7.py
+++ 2019/day7.py
@@ -21,26 +21,30 @@ print('PART 1: ', maxOut)
 
 #PART 2
 
-nums = list(map(int, inputArr))
-
 iters = list(permutations(range(5, 10)))
 maxOut = 0
 
-output = [0]
+output = 0
 
 for it in iters:
-    numArr = [nums.copy()] * 5
+    num1 = nums.copy()
+    num2 = nums.copy()
+    num3 = nums.copy()
+    num4 = nums.copy()
+    num5 = nums.copy()
+    numArr = [num1, num2, num3, num4, num5]
     iArr = [0] * 5
+    inputArr = [[i] for i in it]
+    inputArr[0] += [0]
     outNum = 0
     j = 0
-    while output != ['HALT']:
-        outputTuple = intCode(numArr[j], [it[j], output[0]], iArr[j])
-        outNum = output[0]
-        output = outputTuple[0]
-        iArr[j] = outputTuple[1]
+    while 'HALT' not in inputArr[0]:
+        output = intCode(numArr[j], inputArr[j], iArr[j])
+        inputArr[(j + 1) % 5] += output[0]
+        iArr[j] = output[1]
         j = (j + 1) % 5
 
-    if outNum > maxOut:
-        maxOut = outNum
+    if inputArr[0][0] > maxOut:
+        maxOut = inputArr[0][0]
 
 print('PART 2: ', maxOut)
