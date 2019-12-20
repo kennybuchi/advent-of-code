2023643a232c4d10dcf0d123ab80039df3ddcb89
kennybuchi <lee.kenny81@gmail.com>
2019-12-19 21:08:05 -0800

    2019 - day18 part 1

--- 2019/day18.py
+++ 2019/day18.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 inputFile = open('./2019/day18input.txt', 'r')
 inputText = inputFile.read()
@@ -19,8 +20,11 @@ for i in range(1, length - 1):
         if maze[i][j] != '#':
             graph.add_node((i, j))
             #get key/door/start location
-            if maze[i][j].islower() or maze[i][j] == '@':
+            if maze[i][j] == '@':
                 keys[maze[i][j]] = (i, j)
+            elif maze[i][j].islower():
+                keys[maze[i][j]] = (i, j)
+                doors[(i, j)] = maze[i][j].lower()
             elif maze[i][j].isupper():
                 doors[(i, j)] = maze[i][j].lower()
 
@@ -37,23 +41,25 @@ blockers = {}
 
 for key in keys.keys():
     distances[key] = {}
-    blockers[key] = {}
-    for key2 in keys.keys():
-        if key == key2:
-            continue
-        blockers[key][key2] = set()
+    blockers[key] = set('@')
 
 for key in keys.keys():
     for key2 in keys.keys():
         if key == key2:
             continue
         path = nx.bidirectional_shortest_path(graph, keys[key], keys[key2])
-        for pair in path:
-            if pair in doors or pair in keys:
-                blockers[key][key2].add(doors[pair])
-                blockers[key2][key].add(doors[pair])
+        if key == '@':
+            for i in range(len(path) - 1):
+                pair = path[i]
+                if pair in doors:
+                    blockers[key2].add(doors[pair])
         distances[key][key2] = len(path) - 1
         distances[key2][key] = len(path) - 1
+
+# for key in keys.keys():
+#     if key == '@':
+#         continue
+#     blockers['@'][key].add('@')
 
 def getTotalDistance(arr, distances):
     dist = 0
@@ -69,29 +75,47 @@ toVisitNew = set()
 toVisit.add('@')
 
 
+
 dependencies = nx.DiGraph()
+
 for key in keys:
     dependencies.add_node(key)
 
 while len(visited) < len(keys):
     toVisitNew.clear()
+    for v in toVisit:
+        visited.add(v)
     for item in toVisit:
-        for v in toVisit:
-            visited.add(v)
         for key in keys:
             if key == item or key in visited:
                 continue
 
-            for v in visited:
-                if v in blockers[item][key]:
-                    blockers[item][key].remove(v)
-
-            if len(blockers[item][key]) == 0:
-                dependencies.add_edge(item, key)
+            if key not in toVisitNew and blockers[key].issubset(set(toVisit)):
+                for it in blockers[key]:
+                    dependencies.add_edge(it, key)
                 toVisitNew.add(key)
+
+    for key in keys:
+        for v in toVisit:
+            if v in blockers[key]:
+                blockers[key].remove(v)
 
     toVisit = toVisitNew.copy()
 
+# rework this, too slow
 
-abc = list(nx.topological_sort(dependencies))
-print(getTotalDistance(abc, distances))
+topological = list(nx.all_topological_sorts(dependencies))
+
+shortest = []
+shortestSteps = 999999
+
+totalPaths = len(topological)
+for p in topological:
+    steps = getTotalDistance(p, distances)
+    if steps < shortestSteps:
+        shortest = p
+        shortestSteps = steps
+
+
+print(shortest)
+print(shortestSteps)
